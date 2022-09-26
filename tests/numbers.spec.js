be3a84f725dbe877ade99bd3398a11025bcd0772
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-09-26 20:36:06 -0300

    Projeto - Requisito 4 "Numbers"

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -16,10 +16,24 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
-    fail('Teste vazio!');
-    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    // Escreva um teste em que a função recebe [' '] e retorna false
+    expect(numbers([2, 3, 4])).toBe(true);
+    expect(numbers([2, 'errado', 5])).toBe(false);
+  });
+
+  it('Verifica se a função `numbers` retorna false', () => {
+    expect(numbers([1, 2, '3', 4, 5])).toBe(false);
+  });
+
+  it('Verifica se a função `numbers` retorna false para array número e string', () => {
+    expect(numbers([1, 'a', 3])).toBe(false);
+  });
+
+  it('Verifica se a função `numbers` retorna false para array de string vazio', () => {
+    expect(numbers([' '])).toBe(false);
   });
 });
+
+// Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+// Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+// Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+// Escreva um teste em que a função recebe [' '] e retorna false
